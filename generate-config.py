83db888be3d32634acff1e5babc748d966a62ab7
Dan Chlopek <1285500+Danimal4326@users.noreply.github.com>
2024-12-02 13:07:57 -0600

    fixing typo

--- generate-config.py
+++ generate-config.py
@@ -21,7 +21,7 @@ def main():
     generate_parser.add_argument("--username", required=True, help="PIA username")
     generate_parser.add_argument("--password", required=True, help="PIA password")
     generate_parser.add_argument("--location", required=True, help="PIA server location (i.e., UK-London or 'UK London')")
-    generate_parser.add_argument("--output-dir", help="Directory to save the configuration file (i.e. /etc/wireguard/, default: current directory)"
+    generate_parser.add_argument("--output-dir", help="Directory to save the configuration file (i.e. /etc/wireguard/, default: current directory")
 
     args = parser.parse_args()
 
